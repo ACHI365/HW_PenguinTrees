34bcabfc15314ed62429c12110616ba495281b43
ACHI365 <77834636+ACHI365@users.noreply.github.com>
2022-05-03 14:22:44 +0400

    Add files via upload

---
+++ fop/w8trees/Main (1).java
@@ -0,0 +1,31 @@
+package fop.w8trees;
+
+
+
+import static fop.w8trees.PenguinTrees.getAgeTree;
+import static fop.w8trees.PenguinTrees.getHeightTree;
+
+class Main {
+
+    public static void main(String[] args) {
+//        Tree<Penguin> tree = getHeightTree();
+//
+//        tree.insert(Penguin.p1);
+//        tree.insert(Penguin.p1);
+//        tree.insert(Penguin.p1);
+//        tree.insert(Penguin.p1);
+//        tree.insert(Penguin.p1);
+//
+//        tree.insert(Penguin.p2);
+//        tree.insert(Penguin.p3);
+//        tree.insert(Penguin.p4);
+//        tree.insert(Penguin.p5);
+//
+//        System.out.println(tree.size());
+//        System.out.println(tree.toString());
+//        System.out.println(tree.contains(new Penguin("P", 10, 2)));
+//        System.out.println(tree.countMatches(p -> p.getName().equals("Paul")));
+
+    }
+
+}
---
+++ fop/w8trees/PenguinTrees.java
@@ -0,0 +1,38 @@
+package fop.w8trees;
+
+import java.util.Comparator;
+
+public class PenguinTrees {
+
+    public static Tree<Penguin> getAgeTree() {
+        return new Tree<Penguin>(new Comparator<Penguin>() {
+
+            @Override
+            public int compare(Penguin p1, Penguin p2) {
+                String s = "true";
+                return p1.getAge() - p2.getAge();
+            }
+
+        });
+    }
+
+    public static Tree<Penguin> getHeightTree() {
+        return new Tree<Penguin>(new Comparator<Penguin>() {
+
+            @Override
+            public int compare(Penguin p1, Penguin p2) {
+                return (int) Math.signum(p2.getHeight() - p1.getHeight());
+            }
+
+        });
+    }
+
+    public static int getMinorsAmount(Tree<Penguin> tree) {
+        return tree.countMatches(p -> p.getAge() < 5);
+    }
+
+    public static Object[] getEvenNames(Tree<Penguin> tree) {
+        return tree.getAll(p -> p.getName().length() % 2 == 0);
+    }
+
+}
